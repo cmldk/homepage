7626e2f90d9f08653f1ae82a172ade89388bbdc1
cmldk <cemal.dak@gmail.com>
2023-07-08 22:15:37 +0300

    babel conf & display all bookmarks

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,21 +1,17 @@
 import { format, parse } from 'date-fns';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { RiArrowDownSLine } from 'react-icons/ri';
 import { BOOKMARK_GROUP_FORMAT } from '../../lib/constants';
 import { bookmarkGroupByMounth } from '../../lib/helper';
 import SearchInput from '../Base/SearchInput/SearchInput';
 import { useData } from '../../DataProvider';
-import BookmarkButton from './BookmarkButton';
 import ListDisclosure from './Disclosure';
 
 let searchTimeout = null;
 
 export default function Bookmarks() {
-  const [displayAll, setDisplayAll] = useState(false);
   const [searchValue, setSearchValue] = useState('');
   const [searchedBookmarks, setSearchedBookmarks] = useState();
-  const initialDisplayNumber = 7;
 
   const { bookmarks } = useData();
   const { t } = useTranslation();
@@ -66,34 +62,8 @@ export default function Bookmarks() {
   };
 
   const getBookmarkCards = () => {
-    let filteredBookmarks = {};
-    let filteredDates = [];
-    let bookmarkCount = 0;
-
-    if (displayAll) {
-      filteredDates = sortedDates();
-      filteredBookmarks = searchedBookmarks;
-    } else {
-      const dates = sortedDates();
-      for (let index = 0; index < dates.length; index++) {
-        if (bookmarkCount > initialDisplayNumber) {
-          break;
-        }
-
-        const date = dates[index];
-        const values = searchedBookmarks[date];
-
-        if (values) {
-          filteredBookmarks[date] = values.slice(
-            0,
-            initialDisplayNumber - bookmarkCount
-          );
-          bookmarkCount += filteredBookmarks[date].length;
-
-          filteredDates.push(date);
-        }
-      }
-    }
+    const filteredDates = sortedDates();
+    const filteredBookmarks = searchedBookmarks;
 
     return (
       <>
@@ -102,20 +72,6 @@ export default function Bookmarks() {
             <ListDisclosure date={date} bookmarks={filteredBookmarks[date]} />
           </div>
         ))}
-        {!displayAll && bookmarkCount >= initialDisplayNumber && (
-          <div className="flex items-center mt-12">
-            <BookmarkButton onClick={() => setDisplayAll(true)}>
-              {t('bookmark_show_all')}
-            </BookmarkButton>
-
-            <span
-              onClick={() => setDisplayAll(true)}
-              className="ml-2 cursor-pointer"
-            >
-              <RiArrowDownSLine className="text-dark dark:text-gray-400" />
-            </span>
-          </div>
-        )}
       </>
     );
   };
