d9a0098a3f89ae9d2c2e2e49008ddd0528f09bf3
cmldk <cemal.dak@gmail.com>
2024-10-13 02:30:47 +0300

    github link added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import Social from '../Social/Social';
+import { Trans } from 'react-i18next';
+import { useData } from '../../DataProvider';
 
 export default function Footer() {
+  const { about } = useData();
+
   return (
     <section className="mt-10 font-['regular']">
       <hr className="border-dark dark:border-gray-300" />
@@ -8,6 +12,24 @@ export default function Footer() {
         <span className="flex items-center order-2 md:order-1">
           <Social iconClassName={'mr-4'} />
         </span>
+
+        {about?.project_github && (
+          <p className="text-dark dark:text-gray-400 text-sm order-1 md:order-2 md:ml-auto mt-2 md:mt-0">
+            <Trans
+              i18nKey={'footer_github'}
+              components={[
+                <a
+                  href={about.project_github}
+                  className="underline underline-offset-4 hover:text-portakal"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>,
+              ]}
+            />
+          </p>
+        )}
       </div>
     </section>
   );
