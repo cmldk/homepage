60cad8937fd0ad3c3bf1d4adf5de33216a5d67f0
cmldk <cemal.dak@gmail.com>
2025-08-08 12:14:54 +0300

    netlify update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://cmldk.dev',
-  output: 'static',
+  output: 'server',
   integrations: [tailwind(), mdx(), sitemap()],
+  adapter: netlify(),
 });
